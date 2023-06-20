80df8029742681278ea046f5410b310bba1eecec
LeeHYn <jemie9812@gmail.com>
2023-06-20 22:01:49 +0900

    no message

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -7,6 +7,7 @@
             <br>
             <button @click="showText2">Click me</button>
             <p v-if="textVisible2">{{ text2 }}</p>
+            <p>dddddddd</p>
         </div>
     </div>
 </template>
