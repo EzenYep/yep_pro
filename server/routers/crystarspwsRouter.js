69e4a595091a1b2f01675110f086a953fc5b8321
LeeHYn <105833125+LeeHYn@users.noreply.github.com>
2023-06-28 17:15:48 +0900

    Delete crystarspwsRouter.js

--- server/routers/crystarspwsRouter.js
+++
@@ -1,9 +0,0 @@
-const crystarspwsController = require("../controllers/crystarspwsController.js");
-
-const crystarspwsRouter = require("express").Router();
-
-
-
-crystarspwsRouter.get("/user/crystarspws", crystarspwsController.crystarspws);
-
-module.exports = crystarspwsRouter;
