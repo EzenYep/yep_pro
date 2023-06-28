d84b91761fa6885d26172037ae27171bf1d8a69b
bunmock <136395886+bunmock@users.noreply.github.com>
2023-06-28 17:13:59 +0900

    Delete CrystalEventRouter.js

--- server/routers/CrystalEventRouter.js
+++
@@ -1,9 +0,0 @@
-const CrystalEventController = require("../controllers/CrystalEventController.js");
-
-const CrystalEventRouter = require("express").Router();
-
-
-
-CrystalEventRouter.get("/user/CrystalEvent", CrystalEventController.CrystalEvent);
-
-module.exports = CrystalEventRouter;
