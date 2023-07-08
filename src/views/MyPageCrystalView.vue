968af4d50cd5e6d29dca6777cd6185aebb0bb9eb
bunmock <136395886+bunmock@users.noreply.github.com>
2023-07-08 16:34:57 +0900

    Update MyPageCrystalView.vue
    예매와 관련된 기능제외 회원정보에 대한 기능 모두 구현된 상태입니다.

--- src/views/MyPageCrystalView.vue
+++ src/views/MyPageCrystalView.vue
@@ -1,4 +1,5 @@
 <template>
+<div>
     <div class="container">
         <br>
         <div class="row" style = "background-color :gray">
@@ -36,14 +37,14 @@
             </div>
         </div>
 
-        <div class="row" style="background-color : #000000">/* 빈공간(보이기 쉽게 컬러적용) */
+        <div class="row" style="background-color : background-color:rgba(0,0,0,0);">
             <br><br>
         </div>
 
     </div>
     <div class="row">
-        <div class="row2"  style = "width:15%;height:800px; background-color :rgba(0,0,0,0);" > </div>
-        <div class="row2"  style = "width:20%;height:800px; background-color :gray;" >
+        <div class="row2"  style = "width:15%;height:600px; background-color :rgba(0,0,0,0);" > </div>
+        <div class="row2"  style = "width:20%;height:600px; background-color :gray;" >
 
             <br>
             <div style="width:100%; height:100%; border:1px; float:left; padding:10px;">
@@ -56,16 +57,16 @@
                 <div style="width:100% height:250px; border:1px; float:center; font-size: 18px;">이메일</div>
                 <hr>
                 <div style="width:100% height:250px; border:1px; float:center; font-size: 18px;">비밀번호</div>
-                <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
-                <button type="button" class="btn btn-danger" style="font-size:18px;">탈퇴하기</button>
+                <br><br>
+                <button type="button" class="btn btn-danger" style="font-size:18px;" @click="confirmDeleteMember" >탈퇴하기</button>
             </div>
         </div>
 
-        <div class="row2"  style = "width:5%;height:800px; background-color:gray;" >
+        <div class="row2"  style = "width:5%;height:600px; background-color:gray;" >
 
             <!-- 세로줄 -->
-            <div style="width:75%; height:100%; borde:1px; float:left;">
-                <div class="d-flex" style="height: 100%; z-index: 1;">
+            <div style="width:75%; height:600px; borde:1px; float:left;">
+                <div class="d-flex" style="height: 600px; z-index: 1;">
                     <div class="vr"></div>
 
 
@@ -74,47 +75,123 @@
         </div>
 
 
-        <div class="row2"  style = "width:45%;height:800px; background-color:gray;" >
+        <div class="row2"  style = "width:45%;height:600px; background-color:gray;" >
             <div style="width:100%; height:100%; border:1px; float:left; padding:10px;">
                 <br>
                 <div style="width:100% height:220px; border:1px; text-align:left; font-size: 18px;">{{my_data.member_name}}</div>
                 <hr>
                 <div style="width:100% height:250px; border:1px; text-align:left; font-size: 18px;">{{my_data.birthday}}</div>
                 <hr>
-                <div style="width:100% height:250px; border:1px; text-align:left; font-size: 18px;">{{my_data.member_tel}}
-                    <button type="button" class="btn btn-outline-light" style="float:right">전화번호 변경</button></div>
+                <div style="width:100% height:250px; border:1px; text-align:left; font-size: 18px;">{{my_data.tel}}</div>
                 <hr>
                 <div style="width:100% height:250px; border:1px; text-align:left; font-size: 18px;">{{email}}</div>
                 <hr>
-                <div style="width:100% height:250px; border:1px; text-align:left; font-size: 18px;"><button type="button" class="btn btn-success">비밀번호 변경</button></div>
+                <div style="width:100% height:250px; border:1px; text-align:left; font-size: 18px;">
+                  
+                  <button type="button" class="btn btn-success" @click="showChangePasswordForm">비밀번호 변경</button></div>
+                <form @submit="changePassword">
+                    <br>
+                <div style="width:100% height:250px; border:1px; text-align:left; font-size: 18px;">현재 비밀번호
+                    <input type="password" style="float:right" size="25" v-model="currentPassword" nid="currentPassword" required></div>
                 <br>
-                <div style="width:100% height:250px; border:1px; text-align:left; font-size: 18px;">현재 비밀번호 <input type='text' style="float:right" size="30"></div>
+                <div v-if="changePasswordFormVisible" style="width:100% height:250px; border:1px; text-align:left; font-size: 18px;">새로운 비밀번호
+                    <input type='password' style="float:right" size="25" v-model="newPassword" id="newPassword" required></div>
                 <br>
-                <div style="width:100% height:250px; border:1px; text-align:left; font-size: 18px;">새로운 비밀번호 <input type='text' style="float:right" size="30"></div>
-                <br>
-                <div style="width:100% height:250px; border:1px; text-align:left; font-size: 18px;">새로운 비밀번호 확인 <input type='text' style="float:right" size="30"></div>
-                <br><br><br><br><br><br><br><br><br><br><br>
+                <div v-if="changePasswordFormVisible" style="width:100% height:250px; border:1px; text-align:left; font-size: 18px;">새로운 비밀번호 확인
+                    <input type='password' style="float:right" size="25" v-model="confirmPassword" id="confirmPassword" required></div>
+                <br><br>
 
-                <button type="button" class="btn btn-info" style="font-size:20px; padding:6px 45px 6px 45px;">수정</button><div class="hello"></div> <button type="button" class="btn btn-light" style="font-size:20px; padding:6px 45px 6px 45px;">변경</button>
+                <button v-if="changePasswordFormVisible" type="submit" class="btn btn-info" style="font-size:20px; padding:6px 45px 6px 45px;" @click="updatePassword">수정</button><div class="hello"></div>
+                <button v-if="changePasswordFormVisible" type="button" class="btn btn-light" style="font-size:20px; padding:6px 45px 6px 45px;" @click="cancelChangePassword">취소</button>
+                </form>
             </div>
         </div>
+        
     </div>
-    <div class="row" style="background-color : #000000">/* 빈공간(보이기 쉽게 컬러적용) */
+        <div class="row" style="background-color : background-color:rgba(0,0,0,0);">
         <br><br>
     </div>
+</div>
 </template>
 
+
 <script setup>
+import { useRouter } from 'vue-router';
+import VueCookie from 'vue-cookie';
+import { useStore } from 'vuex';
 import router from '@/router';
 import axios from "axios";
-import store from "@/store/store";
-import {reactive} from "vue";
+import {reactive, ref} from "vue";
+
+const changePasswordFormVisible = ref(false);
+const currentPassword = ref('');
+const newPassword = ref('');
+const confirmPassword = ref('');
+const store = useStore();
+
+
+// 비밀번호 변경을 누르면 폼을 표시합니다.
+function showChangePasswordForm() {
+  changePasswordFormVisible.value = true;
+}
+
+function cancelChangePassword() {
+  changePasswordFormVisible.value = false;
+}
+
+// 비밀번호 변경
+async function updatePassword(email) {
+  if (newPassword.value !== confirmPassword.value) {
+    console.error('새로운 비밀번호와 확인 비밀번호가 일치하지 않습니다.');
+    return;
+  }
+
+  const passwordData = {
+    currentPassword: currentPassword.value,
+    newPassword: newPassword.value,
+    email: email,
+  };
+
+  try {
+    const response = await axios.post('http://localhost:9212/api/update-password', passwordData);
+    console.log('비밀번호 업데이트 성공');
+    // 비밀번호 업데이트 성공 처리 로직 추가
+  } catch (error) {
+    console.error('비밀번호 업데이트 실패', error);
+    // 비밀번호 업데이트 실패 처리 로직 추가
+  }
+}
+
+
+// 회원 삭제 확인
+
+const confirmDeleteMember = async () => {
+  const confirmed = confirm("정말로 회원을 탈퇴하시겠습니까?");
+
+  if (confirmed) {
+    const memberEmail = store.state.email;
+
+    try {
+        const response = await axios.post('http://localhost:9212/api/delete-member', { memberEmail });
+      console.log('회원 삭제 성공');
+      // 회원 삭제 성공 처리 로직 추가
+
+      store.commit('CLEAR_TOKEN'); // 토큰 삭제
+      await router.replace({ name: 'home' }); // HomepageMainView 페이지로 이동
+
+    } catch (error) {
+      console.error('회원 삭제 실패', error);
+      // 회원 삭제 실패 처리 로직 추가
+    }
+  }
+}
 
 const my_data = reactive({
     member_name:'',
-    member_tel:'',
+    tel:'',
     birthday: ''
 })
+
 const email = store.state.email;
 const myPage = async ()=>{
     const body = {email : store.state.email}
@@ -122,8 +199,8 @@ const myPage = async ()=>{
     const code = res.data.code
     if(code === 200){
         my_data.member_name = res.data.member_name;
-        my_data.member_tel = res.data.member_tel;
-        my_data.birthday = res.data.member_birthday;
+        my_data.tel = res.data.tel;
+        my_data.birthday = res.data.birthday;
     }else if(code === 400){
         alert("사용자 조회에 실패하셧습니다.")
     }
